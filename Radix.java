d01df136180325248efe1c63057e59212e38ba3f
Jackie Lin <jlin00@stuy.edu>
2019-04-01 09:51:27 -0400

    added buckets

--- Radix.java
+++ Radix.java
@@ -1,5 +1,17 @@
 public class Radix{
-  public void radixsort(int[]data){
+  public void radixsort(int[] data){
+    MyLinkedList<Integer> zero = new MyLinkedList();
+    MyLinkedList<Integer> one = new MyLinkedList();
+    MyLinkedList<Integer> two = new MyLinkedList();
+    MyLinkedList<Integer> three = new MyLinkedList();
+    MyLinkedList<Integer> four = new MyLinkedList();
+    MyLinkedList<Integer> five = new MyLinkedList();
+    MyLinkedList<Integer> six = new MyLinkedList();
+    MyLinkedList<Integer> seven = new MyLinkedList();
+    MyLinkedList<Integer> eight = new MyLinkedList();
+    MyLinkedList<Integer> nine = new MyLinkedList();
+
     
+
   }
 }
