91ba71688375f6d44a8c6d50e615f9da595caaf0
Jackie Lin <jlin00@stuy.edu>
2019-04-02 00:42:56 -0400

    added comments

--- Radix.java
+++ Radix.java
@@ -1,16 +1,18 @@
+@SuppressWarnings({"unchecked", "rawtypes"})
+
 public class Radix{
   public static void radixsort(int[] data){
-    MyLinkedList<Integer>[] buckets = new MyLinkedList[10];
-    int passes = max(data);
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[10]; //makes ten buckets for digits 0-9
+    int passes = max(data); //finds number of digits in maximum value of data
   }
 
   public static int max(int[] data){
-    if (data.length <= 0) return 0;
-    int max = data[0];
+    if (data.length == 0) return 0; //if no elements, zero passes
+    int max = data[0]; //temporary storage
     for (int i = 0; i < data.length; i++){
-      if (data[i] > max) max = data[i];
+      if (data[i] > max) max = data[i]; //find max
      }
-    return (int)Math.log10(max) + 1;
+    return (int)Math.log10(max) + 1; //integer max + 1
   }
 
   public static void main(String[] args) {
